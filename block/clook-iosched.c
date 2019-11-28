eb90da6fe3ca42df67a495275a82b8dd8e3563df
Marvin Torres <mtorr068@fiu.edu>
2019-11-27 21:36:15 -0500

    Update clook-iosched.c

--- block/clook-iosched.c
+++ block/clook-iosched.c
@@ -93,7 +93,7 @@ static void clook_add_request(struct request_queue *q, struct request *rq)
 			curr_request = list_entry(itr, struct request, queuelist);
 			curr_sector = blk_rq_pos(curr_request);
 			if (curr_sector >= head && sector <= curr_sector) {
-				list_add_tail(&rq->queuelist, &itr);
+				list_add_tail(&rq->queuelist, &itr->queue);
 				return;
 			}
 		}
@@ -102,7 +102,7 @@ static void clook_add_request(struct request_queue *q, struct request *rq)
 			curr_request = list_entry(itr, struct request, queuelist);
 			curr_sector = blk_rq_pos(curr_request);
 			if (curr_sector < head && sector <= curr_sector) {
-				list_add_tail(&rq->queuelist, &itr);
+				list_add_tail(&rq->queuelist, &itr->queue);
 				return;
 			}
 		}
